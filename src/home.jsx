3b3320de8c4bd8ab97057946fa57a4101eb5aa81
AbdukarimDev <abdukarimdev@gmail.com>
2026-01-23 20:53:19 +0900

    update

--- src/home.jsx
+++ src/home.jsx
@@ -1,22 +1,39 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { BackBtn } from './button';
+import SearchIcon from "./assets/Search.svg";
+import BgrImg from "./assets/img-div.jpg"
+
 
 const HomeComponent = () => {
-    const navigate = useNavigate();
-function backFunction() {
-    console.log("back ")
-    navigate(-1);
+  return(
+  <div className='Container'> 
+  <div className='navbar-wrapper'>
+    <div className='navbar'>
+      <div className='logo'>ArchiWeb</div>
 
-}
+      <div className='menu'>
+        <p>Home</p>
+        <p>Projects</p>
+        <p>About</p>
+        <p>Contact</p>
+      </div>
 
-  return (
-    <>
-    <button onClick={backFunction}>BACK</button>
-    <div style={{ display: "flex", }}>HomePage</div>
-    <button onClick={BackBtn}>BackBtn</button>
-  </>
-  )
-}
+      <div className='icon'> 
+        <img src={SearchIcon} alt="SearchIcon" />
+      </div>
+    </div>
+
+  </div>  
+
+  <div className='hero'>
+    <img src={BgrImg} alt="" />
+    <div className='overlay'>
+      <h1>Modern Architecture</h1>
+    </div>
+  </div>
+  </div>  //container
+  );
+};
 
 export default HomeComponent;
